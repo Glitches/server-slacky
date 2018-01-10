9564e548a97c85130d1759457c00ec1cbb012501
Andrea Ceccarelli <gltch2003@gmail.com>
2018-01-10 17:52:01 +0100

    My first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+
+require('dotenv').config();
+const express = require('express');
+const request = require('request');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app
+    .get('/auth', (req, res) => {
+        res.sendFile(__dirname + '/add_to_slack.html');
+    })
+    .get('/auth/redirect', (req, res) => {
+        var options = {
+            uri: 'https://slack.com/api/oauth.access?code='
+      + req.query.code +
+      '&client_id=' + process.env.CLIENT_ID +
+      '&client_secret=' + process.env.CLIENT_SECRET +
+      '&redirect_uri=' + process.env.REDIRECT_URI,
+            method: 'GET'
+        };
+        request(options, (error, response, body) => {
+            var JSONresponse = JSON.parse(body);
+            if (!JSONresponse.ok) {
+                console.log(JSONresponse);
+                res.send("Error encountered: \n" + JSON.stringify(JSONresponse)).status(200).end();
+            } else {
+                console.log(JSONresponse);
+                res.send("Success!");
+            }
+        });
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
