daff72a8b71a4191c461dae579962733bef6f9be
Arol <arolet@gmail.com>
2018-01-15 12:02:11 +0100

    feat: validating oauth code

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const express = require('express');
 const request = require('request');
 const app = express();
 const https = require('https');
+const qs = require('qs');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000
 
 app
+    .use(cors())
     .get('/auth', (req, res) => {
         console.log(req.originalUrl);
         res.sendFile(__dirname + '/static/add_to_slack.html');
@@ -31,5 +34,23 @@ app
                 res.send(`Success! ${JSONresponse.access_token}`);
             }
         });
+    })
+    .get('/validate', (req, res) => {
+      // http://host/path?code=123
+      const { code } = qs.parse(req.url.split('?')[1]);
+      const options = {
+        uri: 'https://slack.com/api/oauth.access?' +
+          qs.stringify({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            code,
+            redirect_uri: process.env.REDIRECT_URI
+          }),
+      }
+      request(options, (error, response, body) => {
+        if(error) return console.error(errror);
+
+        res.json(body);
+      })
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
